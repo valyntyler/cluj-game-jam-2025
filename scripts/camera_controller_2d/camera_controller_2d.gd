736649648b8ad832d02483e0068db322e6d5c9fd
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-09 21:41:58 +0300

    feat: camera deadzone box

--- scripts/camera_controller_2d/camera_controller_2d.gd
+++ scripts/camera_controller_2d/camera_controller_2d.gd
@@ -1,10 +1,19 @@
+@tool
 class_name CameraController2D
 extends Camera2D
 
+@export var bounds: Rect2i:
+	set(value):
+		bounds = value
+		queue_redraw()
+
+@export_group("Zooming")
 @export var zoom_sensitivity: float = 0.1
 @export var zoom_smoothing: float = 0.2
 @export var min_zoom: float = 0.4
 @export var max_zoom: float = 4.0
+
+@export_group("Movement")
 @export var move_speed: float = 0.5
 @export var centering_force: float = 1.0
 
@@ -12,15 +21,28 @@ var zoom_scale: float = 1.0
 var move_delta: Vector2 = Vector2.ZERO
 
 
+func _draw():
+	if Engine.is_editor_hint():
+		draw_rect(bounds, Color(0.5, 0.8, 0.8, 1), false)
+
+
 func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
 		position -= (move_delta * move_speed) / zoom_scale
 
-	position = position.lerp(Vector2.ZERO, delta * centering_force / zoom_scale)
+	if not bounds.has_point(position):
+		position = position.lerp(Vector2.ZERO, delta * centering_force / zoom_scale)
+
 	zoom = zoom.lerp(Vector2.ONE * zoom_scale, delta / zoom_smoothing)
 
 
 func _input(event):
+	if Engine.is_editor_hint():
+		return
+
 	match event:
 		var motion when motion is InputEventMouseMotion:
 			move_delta = motion.relative
