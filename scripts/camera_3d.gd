c185e06381f5456bc0cfd350bc34bcf7e1249331
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-15 00:20:08 +0300

    feat: screenspace raycasting

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -1,7 +1,19 @@
 extends Camera3D
 
+var ray_origin: Vector3
+var ray_end: Vector3
+
 
 func _process(_delta: float) -> void:
-	var mouse_pos = get_viewport().get_mouse_position()
-	var world_pos = project_position(mouse_pos, position.z)
-	print(world_pos)
+	var space_state = get_world_3d().direct_space_state
+	var mouse_position = get_viewport().get_mouse_position()
+
+	ray_origin = project_ray_origin(mouse_position)
+	ray_end = project_ray_normal(mouse_position) * 2000.0
+
+	var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
+	var intersection = space_state.intersect_ray(query)
+
+	if not intersection.is_empty():
+		print("relief")
+		DebugDraw3D.draw_sphere(intersection.position, 0.01, Color.RED)
