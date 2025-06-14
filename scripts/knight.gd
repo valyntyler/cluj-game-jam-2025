3fd388ec964d3f7eb1444b1a1ff864d9154a6459
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-15 01:48:00 +0300

    feat: highlight color

--- scripts/knight.gd
+++ scripts/knight.gd
@@ -1,10 +1,12 @@
 extends StaticBody3D
 
 var hover := false
+@onready var knight: MeshInstance3D = $Knight
 
 
 func _process(_delta: float) -> void:
 	print(hover)
+	knight.material_override.albedo_color = Color.RED if hover else Color.WHITE
 
 
 func _on_mouse_entered() -> void:
