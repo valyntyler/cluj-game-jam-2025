4091ab3f2390e105034260147f2eb48a170aefd4
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-15 21:20:37 +0300

    fmt: gdformat

--- classes/Clickable.gd
+++ classes/Clickable.gd
@@ -1,17 +1,23 @@
 class_name Clickable extends StaticBody3D
 
-var hover := false;
+var hover := false
+
 
 func _ready() -> void:
 	mouse_entered.connect(on_mouse_entered)
 	mouse_exited.connect(on_mouse_exited)
 	input_event.connect(on_input_event)
-	
+
+
 func on_mouse_entered() -> void:
 	pass
+
 
 func on_mouse_exited() -> void:
 	pass
 
-func on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+
+func on_input_event(
+	camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int
+) -> void:
 	pass
--- scripts/grid.gd
+++ scripts/grid.gd
@@ -1,23 +1,25 @@
-extends Node;
+extends Node
 
-@export var GRID_SIZE_X: int = 4;
-@export var GRID_SIZE_Z: int = 4;
-@export var GRID_POSITION_Y: float = 1.0;
-@export var TILE_SIZE: float = 2.0;
-@export var TILE_SCENE: PackedScene;
+@export var GRID_SIZE_X: int = 4
+@export var GRID_SIZE_Z: int = 4
+@export var GRID_POSITION_Y: float = 1.0
+@export var TILE_SIZE: float = 2.0
+@export var TILE_SCENE: PackedScene
+
 
 func _ready() -> void:
-	generate_grid();
+	generate_grid()
+
 
 func generate_grid() -> void:
-	var offset_x = (GRID_SIZE_X * TILE_SIZE) / 2.0;
-	var offset_z = (GRID_SIZE_Z * TILE_SIZE) / 2.0;
+	var offset_x = (GRID_SIZE_X * TILE_SIZE) / 2.0
+	var offset_z = (GRID_SIZE_Z * TILE_SIZE) / 2.0
 
 	for x in GRID_SIZE_X:
 		for z in GRID_SIZE_Z:
-			var tile_instance = TILE_SCENE.instantiate();
-			var pos = Vector3(x * TILE_SIZE, GRID_POSITION_Y, z * TILE_SIZE);
-			pos.x -= offset_x;
-			pos.z -= offset_z;
-			tile_instance.position = pos;
-			add_child(tile_instance);
+			var tile_instance = TILE_SCENE.instantiate()
+			var pos = Vector3(x * TILE_SIZE, GRID_POSITION_Y, z * TILE_SIZE)
+			pos.x -= offset_x
+			pos.z -= offset_z
+			tile_instance.position = pos
+			add_child(tile_instance)
