e9ce3d4259454af576928228e502a6676a743435
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-15 21:13:23 +0300

    fmt: unit.gd

--- scripts/unit.gd
+++ scripts/unit.gd
@@ -3,14 +3,19 @@ extends Clickable
 @onready
 var mesh: MeshInstance3D = get_children().filter(func(c): return c is MeshInstance3D).front()
 
+
 func on_mouse_entered() -> void:
 	mesh.material_override.albedo_color = Color.RED
 	hover = true
+
 
 func on_mouse_exited() -> void:
 	mesh.material_override.albedo_color = Color.WHITE
 	hover = false
 
-func on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+
+func on_input_event(
+	camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int
+) -> void:
 	if event is InputEventMouseButton:
 		mesh.material_override.albedo_color = Color.GREEN
